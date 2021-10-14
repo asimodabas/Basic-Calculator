7bcd8220f456354d338d522ed6e78ee92ea4426c
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-15 02:19:16 +0300

    Initial commit

---
+++ app/src/main/java/com/asimodabas/calculator/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.asimodabas.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textViewSonuc.text = "0"
+        buttonToplama.setOnClickListener {
+          val  alinanSayi1 = editTextSayi1.text.toString()
+          val  alinanSayi2 = editTextSayi2.text.toString()
+            val sayi1 = alinanSayi1.toInt()
+            val sayi2 = alinanSayi2.toInt()
+
+            val toplam = sayi1+sayi2
+            textViewSonuc.text = toplam.toString()
+        }
+        buttonCarpma.setOnClickListener {
+            val  alinanSayi1 = editTextSayi1.text.toString()
+            val  alinanSayi2 = editTextSayi2.text.toString()
+            val sayi1 = alinanSayi1.toInt()
+            val sayi2 = alinanSayi2.toInt()
+
+            val carpma = sayi1*sayi2
+            textViewSonuc.text = carpma.toString()
+        }
+
+    }
+}
